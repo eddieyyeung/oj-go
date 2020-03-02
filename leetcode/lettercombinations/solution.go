be9ada9c4cc1c8e6e45a3d1cee7e0e4bc3530825
eddieyeung <eddie.y.yeung@gmail.com>
2020-03-02 21:30:13 +0800

    feat: solved

---
+++ leetcode/lettercombinations/solution.go
@@ -0,0 +1,38 @@
+package lettercombinations
+
+var digitMap map[string][]string = map[string][]string{
+	"1": []string{},
+	"2": []string{"a", "b", "c"},
+	"3": []string{"d", "e", "f"},
+	"4": []string{"g", "h", "i"},
+	"5": []string{"j", "k", "l"},
+	"6": []string{"m", "n", "o"},
+	"7": []string{"p", "q", "r", "s"},
+	"8": []string{"t", "u", "v"},
+	"9": []string{"w", "x", "y", "z"},
+}
+
+func letterCombinations(digits string) []string {
+	re := []string{}
+	if len(digits) == 0 {
+		return re
+	}
+	bt(digits, "", &re)
+	return re
+}
+
+func bt(digits string, lc string, re *[]string) {
+	if len(digits) == 0 {
+		*re = append(*re, lc)
+		return
+	}
+	letters := digitMap[string(digits[0])]
+	for _, l := range letters {
+		bt(digits[1:], lc+l, re)
+	}
+}
+
+// Run https://leetcode-cn.com/explore/interview/card/top-interview-questions-medium/49/backtracking/91/
+func Run(digits string) []string {
+	return letterCombinations(digits)
+}
