0a0867b1c00e0899c84f22a3796e3fe2485c5a02
eddieyeung <eddie.y.yeung@gmail.com>
2020-03-08 21:15:19 +0800

    feat: Solved 1365. 有多少小于当前数字的数字

---
+++ leetcode/how-many-numbers-are-smaller-than-the-current-number/solution.go
@@ -0,0 +1,19 @@
+package howmanynumbersaresmallerthanthecurrentnumber
+
+// https://leetcode-cn.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+func smallerNumbersThanCurrent(nums []int) []int {
+	r := []int{}
+	for i := 0; i < len(nums); i++ {
+		smaller := 0
+		for j := 0; j < len(nums); j++ {
+			if i == j {
+				continue
+			}
+			if nums[j] < nums[i] {
+				smaller++
+			}
+		}
+		r = append(r, smaller)
+	}
+	return r
+}
---
+++ leetcode/how-many-numbers-are-smaller-than-the-current-number/solution_test.go
@@ -0,0 +1,32 @@
+package howmanynumbersaresmallerthanthecurrentnumber
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_smallerNumbersThanCurrent(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"8,1,2,2,3",
+			args{
+				[]int{8, 1, 2, 2, 3},
+			},
+			[]int{4, 0, 1, 1, 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := smallerNumbersThanCurrent(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("smallerNumbersThanCurrent() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
