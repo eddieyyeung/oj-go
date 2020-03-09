7a1a3dadf2d9e95224e4d0ca6103d074e255cbec
eddieyeung <eddie.y.yeung@gmail.com>
2020-03-09 22:43:19 +0800

    feat: solved 121. 买卖股票的最佳时机

---
+++ leetcode/best-time-to-buy-and-sell-stock/solution.go
@@ -0,0 +1,14 @@
+package besttimetobuyandsellstock
+
+// maxProfit https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
+func maxProfit(prices []int) int {
+	max := 0
+	for i := 0; i < len(prices)-1; i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if diff := prices[j] - prices[i]; diff > max {
+				max = diff
+			}
+		}
+	}
+	return max
+}
