44755f354c3fb2f8b300db9645d00f0942bf340c
eddieyeung <eddie.y.yeung@gmail.com>
2020-03-16 23:02:20 +0800

    feat: solved 146. LRU缓存机制

--- leetcode/lru-cache/solution.go
+++ leetcode/lru-cache/solution.go
@@ -2,69 +2,153 @@
 // https://leetcode-cn.com/problems/lru-cache/
 package lru_cache
 
-import "container/list"
-
-type LRUCache struct {
-	ElementsMap map[int]*list.Element
-	List        *list.List
-	Capacity    int
+type node struct {
+	Key  int
+	Val  int
+	Prev *node
+	Next *node
 }
 
-type Item struct {
-	Key int
-	Val int
+type LRUCache struct {
+	NodesMap map[int]*node
+	Head     *node
+	Tail     *node
+	Len      int
+	Capacity int
 }
 
 func Constructor(capacity int) LRUCache {
+	head := &node{}
+	tail := &node{}
+	head.Next = tail
+	tail.Prev = head
 	return LRUCache{
-		map[int]*list.Element{},
-		list.New(),
-		capacity,
+		NodesMap: map[int]*node{},
+		Head:     head,
+		Tail:     tail,
+		Capacity: capacity,
 	}
 }
 
+func (this *LRUCache) add(n *node) {
+	n.Next = this.Head.Next
+	n.Prev = this.Head
+	this.Head.Next.Prev = n
+	this.Head.Next = n
+	this.NodesMap[n.Key] = n
+	this.Len++
+}
+
+func (this *LRUCache) remove(n *node) {
+	if this.Len == 0 {
+		return
+	}
+	prev := n.Prev
+	next := n.Next
+	prev.Next = next
+	next.Prev = prev
+	delete(this.NodesMap, n.Key)
+	this.Len--
+}
+
+func (this *LRUCache) pop() *node {
+	if this.Len == 0 {
+		return nil
+	}
+	n := this.Tail.Prev
+	this.remove(n)
+	return n
+}
+
 func (this *LRUCache) Get(key int) int {
-	if e, ok := this.ElementsMap[key]; ok {
-		v, _ := e.Value.(Item)
-		this.List.Remove(e)
-		this.List.PushBack(v)
-		this.ElementsMap[key] = this.List.Back()
-		return v.Val
+	if n, ok := this.NodesMap[key]; ok {
+		this.remove(n)
+		this.add(n)
+		return n.Val
 	}
 	return -1
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	if e, ok := this.ElementsMap[key]; ok {
-		delete(this.ElementsMap, key)
-		this.List.Remove(e)
-		this.List.PushBack(Item{
-			key,
-			value,
-		})
-		this.ElementsMap[key] = this.List.Back()
+	if n, ok := this.NodesMap[key]; ok {
+		n.Val = value
+		this.remove(n)
+		this.add(n)
 		return
 	}
-	if this.Capacity == this.List.Len() {
-		e := this.List.Front()
-		v, _ := e.Value.(Item)
-		item := Item{
-			key,
-			value,
-		}
-		delete(this.ElementsMap, v.Key)
-		this.List.Remove(e)
-		this.List.PushBack(item)
-		this.ElementsMap[key] = this.List.Back()
+	n := &node{
+		Key: key,
+		Val: value,
+	}
+	if this.Len == this.Capacity {
+		this.pop()
+		this.add(n)
 	} else {
-		item := Item{
-			key,
-			value,
-		}
-		this.List.PushBack(item)
-		this.ElementsMap[key] = this.List.Back()
+		this.add(n)
 	}
 }
+
+// type LRUCache struct {
+// 	ElementsMap map[int]*list.Element
+// 	List        *list.List
+// 	Capacity    int
+// }
+//
+// type Item struct {
+// 	Key int
+// 	Val int
+// }
+//
+// func Constructor(capacity int) LRUCache {
+// 	return LRUCache{
+// 		map[int]*list.Element{},
+// 		list.New(),
+// 		capacity,
+// 	}
+// }
+//
+// func (this *LRUCache) Get(key int) int {
+// 	if e, ok := this.ElementsMap[key]; ok {
+// 		v, _ := e.Value.(Item)
+// 		this.List.Remove(e)
+// 		this.List.PushBack(v)
+// 		this.ElementsMap[key] = this.List.Back()
+// 		return v.Val
+// 	}
+// 	return -1
+// }
+//
+// func (this *LRUCache) Put(key int, value int) {
+// 	if e, ok := this.ElementsMap[key]; ok {
+// 		delete(this.ElementsMap, key)
+// 		this.List.Remove(e)
+// 		this.List.PushBack(Item{
+// 			key,
+// 			value,
+// 		})
+// 		this.ElementsMap[key] = this.List.Back()
+// 		return
+// 	}
+// 	if this.Capacity == this.List.Len() {
+// 		e := this.List.Front()
+// 		v, _ := e.Value.(Item)
+// 		item := Item{
+// 			key,
+// 			value,
+// 		}
+// 		delete(this.ElementsMap, v.Key)
+// 		this.List.Remove(e)
+// 		this.List.PushBack(item)
+// 		this.ElementsMap[key] = this.List.Back()
+// 	} else {
+// 		item := Item{
+// 			key,
+// 			value,
+// 		}
+// 		this.List.PushBack(item)
+// 		this.ElementsMap[key] = this.List.Back()
+// 	}
+// }
 
 /**
  * Your LRUCache object will be instantiated and called as such:
--- leetcode/lru-cache/solution_test.go
+++ leetcode/lru-cache/solution_test.go
@@ -1,15 +1,29 @@
 package lru_cache
 
 import (
-	"fmt"
 	"testing"
 )
 
 func Test_lru_cache(t *testing.T) {
-	obj := Constructor(2)
-	obj.Put(1, 1)
-	obj.Put(2, 2)
-	fmt.Println(obj.Get(1))
-	obj.Put(3, 3)
-	fmt.Println(obj.Get(2))
+	t.Run("", func(t *testing.T) {
+		obj := Constructor(2)
+		obj.Put(1, 1)
+		obj.Put(2, 2)
+		t.Log(obj.Get(1))
+		obj.Put(3, 3)
+		t.Log(obj.Get(2))
+		obj.Put(4, 4)
+		t.Log(obj.Get(1))
+		t.Log(obj.Get(3))
+		t.Log(obj.Get(4))
+	})
+	t.Run("", func(t *testing.T) {
+		obj := Constructor(2)
+		obj.Put(2, 1)
+		obj.Put(2, 2)
+		t.Log(obj.Get(2))
+		obj.Put(1, 1)
+		obj.Put(4, 1)
+		t.Log(obj.Get(2))
+	})
 }
