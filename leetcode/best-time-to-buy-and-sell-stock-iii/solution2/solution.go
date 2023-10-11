1d4d56f9741efacd99f48eaaed88307724b4ddb0
eddieyeung <eddie.y.yeung@gmail.com>
2023-10-11 22:40:19 +0800

    feat: add doc

--- leetcode/best-time-to-buy-and-sell-stock-iii/solution2/solution.go
+++ leetcode/best-time-to-buy-and-sell-stock-iii/solution2/solution.go
@@ -1,12 +1,13 @@
 package solution2
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
+// 状态转移公式：
+// buy1 = max(buy1, -prices[i])
+// sell1 = max(sell1, buy1+prices[i])
+// buy2 = max(buy2, sell1-prices[i])
+// sell2 = max(sell2, buy2+prices[i])
+//
+// 初始化：
+// buy1, buy2 = -prices[0], -prices[0]
 func maxProfit(prices []int) int {
 	dp := [4]int{-prices[0], 0, -prices[0], 0}
 	for i := 1; i < len(prices); i++ {
@@ -14,4 +15,11 @@ func maxProfit(prices []int) int {
 		dp[0], dp[1], dp[2], dp[3] = max(dp[0], -pricei), max(dp[1], dp[0]+pricei), max(dp[2], dp[1]-pricei), max(dp[3], dp[2]+pricei)
 	}
 	return dp[3]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- leetcode/best-time-to-buy-and-sell-stock-iv/solution2/solution.go
+++ leetcode/best-time-to-buy-and-sell-stock-iv/solution2/solution.go
@@ -7,6 +7,18 @@ func max(a, b int) int {
 	return b
 }
 
+// 参考 123. 买卖股票的最佳时机 III
+// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iii/
+//
+// 状态转移公式：
+// buy1 = max(buy1, -prices[i])
+// sell1 = max(sell1, buy1+prices[i])
+// buy2 = max(buy2, sell1-prices[i])
+// sell2 = max(sell2, buy2+prices[i])
+// ...
+//
+// 初始化：
+// buy1, buy2 = -prices[0], -prices[0]
 func maxProfit(k int, prices []int) int {
 	dp := make([]int, k*2+1)
 	for i := 1; i < k*2; i++ {
